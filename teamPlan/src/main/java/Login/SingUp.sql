218058307991f0c22b593ad241d43b0afe974463
wjdtjddjq123 <59285584+wjdtjddjq123@users.noreply.github.com>
2023-09-15 16:31:39 +0900

    seongeop_singup
    seongeop_singup

---
+++ teamPlan/src/main/java/Login/SingUp.sql
@@ -0,0 +1,7 @@
+CREATE TABLE User (
+    id VARCHAR2(50) PRIMARY KEY,
+    password VARCHAR2(100),
+    name VARCHAR2(50),
+    birthdate DATE,
+    github VARCHAR2(100)
+);
---
+++ teamPlan/src/main/webapp/teamPlan/create.sql
@@ -0,0 +1,8 @@
+create table Login(
+	id 		 varchar2(15),
+	passWd 	 varchar2(20),
+	userName varchar2(20),
+	birth 	 varchar2(20),
+	gitEmail varchar2(20)
+	
+)
